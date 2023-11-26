62bc57f06312a546637fbc5948210fc1cf6218cf
MudassarMukhtar <97121751+MudassarMukhtar@users.noreply.github.com>
2023-11-26 21:08:30 +0000

    CREATE A GAME TO GET VALUE FROM USER
    this code shown the result if a user enter correct number then it show the results on console otherwise user needs to enter correct number.
    i just uploaded javascript file i didt uploaded html file which contain script tag with src attribute with file name .

---
+++ uservalue.js
@@ -0,0 +1,7 @@
+let gamenum=25; 
+let usernum=prompt("enter the number");
+while(gamenum!=usernum)
+{
+    usernum=prompt("please enter correct number guess again");
+}
+console.log("congratulations you entered correct number");
